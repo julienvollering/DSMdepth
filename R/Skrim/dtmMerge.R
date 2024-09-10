62174d402996ccca0057fed7d118ffa720f199c1
Julien Vollering <julienvollering@gmail.com>
2024-09-10 15:02:02 +0200

    merge Skrim DTM tiles

--- R/Skrim/dtmMerge.R
+++ R/Skrim/dtmMerge.R
@@ -1,7 +1,8 @@
-library(terra) #v1.7-37
-files <- list.files("data/Haram Skodje Ørskog Vestnes 2pkt 2015/data/dtm", pattern = ".tif$", full.names = TRUE)
+library(terra) #v1.7.78
+files <- c("data/Skrim/eksport_926374_20240909/dtm1/data/dtm1_33_119_112.tif",
+           "data/Skrim/eksport_926374_20240909/dtm1/data/dtm1_33_120_112.tif")
 tiles <- sprc(files)
 summary(tiles)
 merged <- merge(tiles)
 merged
-writeRaster(merged, "data/DTMmerged.tif")
+writeRaster(merged, "data/Skrim/DTMmerged.tif", overwrite = TRUE)
