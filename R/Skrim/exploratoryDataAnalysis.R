638e7cadfdf54b933f4068f7aa60286276f8fa59
Julien Vollering <julienvollering@gmail.com>
2024-09-13 09:40:12 +0200

    Skrim: EDA

--- R/Skrim/exploratoryDataAnalysis.R
+++ R/Skrim/exploratoryDataAnalysis.R
@@ -6,8 +6,8 @@ library(clhs)
 
 # Spatial structure in peat depth ####
 
-depthpts <- read_csv("data/depth/all.csv") |> 
-  st_as_sf(coords = c('X', 'Y'), crs = 25832)
+depthpts <- read_csv("data/Skrim/depth_all.csv") |> 
+  st_as_sf(coords = c('X', 'Y'), crs = 25833)
 
 # From probe data only
 depth_probe <- depthpts |> 
@@ -28,25 +28,25 @@ emp_variog <- variogram(depth_cm ~ 1, cutoff = 200, width = 5,
                         data = depth_gpr[sample(seq_len(nrow(depth_gpr)), 
                                                 nrow(depth_gpr)*0.1),])
 plot(emp_variog)
-print(emp_variog) # Range of 75 m
-sqrt(140*2) # Nugget at 2.5 m of 16 cm
+print(emp_variog) # Range of 40 m
+sqrt(390*2) # Nugget at 2.8 m of 28 cm
 
 # At modeling resolution: average depth in 10 m cells
-depthcells <- st_read("output/modeling.gpkg", "dataframe")
+depthcells <- st_read("output/Skrim/modeling.gpkg", "dataframe")
 depthcells_sp <- as(depthcells, "Spatial")
 emp_variog <- variogram(depth_cm ~ 1, cutoff = 1000, width = 10, 
                         data = depthcells_sp)
 plot(emp_variog)
-print(emp_variog) # Range of 230 m
-sqrt(9350*2) # Sill at 230 m of 136 cm
-sqrt(760*2) # Nugget at 10 m of 40 cm
+print(emp_variog) # Range of 110 m?
+sqrt(7900*2) # Sill at 110 m of 125 cm
+sqrt(2000*2) # Nugget at 10 m of 63 cm
 # Isolated but similar peatland basins in the landscape can explain semivariance decline beyond range. 
 
 # Sample attributes ####
 
-depthcells <- st_read("output/modeling.gpkg", "dataframe", stringsAsFactors = TRUE)
+depthcells <- st_read("output/Skrim/modeling.gpkg", "dataframe", stringsAsFactors = TRUE)
 count(depthcells, ar5cover)
-count(depthcells, ar5cover, ar5soil)
+count(depthcells, ar5cover, ar5soil) # No organic soil forest or organic soil open area
 count(depthcells, dmkdepth)
 depthcells |> 
   st_drop_geometry() |> 
@@ -66,23 +66,29 @@ depthcells |>
 # Representativeness of the sample ####
 # Representativeness cf. mapped peatland in the study area
 
-predictors <- rast("output/predictors.tif")
+predictors <- rast("output/Skrim/predictors.tif")
 plot(predictors)
 
-ar5.myr <- st_read("data/Orskogfjellet-site.gpkg", layer="mask_ar5myr")
+ar5.myr <- st_read("data/Skrim/Skrim-site.gpkg", layer="mask_ar5myr")
 st_crs(ar5.myr) == st_crs(predictors)
 plot(predictors[['elevation']])
 plot(ar5.myr, add=TRUE)
-
 st_intersects(depthcells, ar5.myr, sparse=FALSE) |> 
   apply(1, any) |> 
   (\(x) !x)() |> 
-  sum() # 438 depth cells centers that do not intersect AR5 myr
+  sum() # 71 depth cells centers that do not intersect AR5 myr
 
-ar5cells <- mask(predictors, ar5.myr)
-plot(ar5cells)
+predictivedomain <- st_read("data/Skrim/Skrim-site.gpkg", "mask_predictivedomain")
+plot(predictors[['elevation']])
+plot(predictivedomain, add=TRUE)
+plot(st_geometry(depthcells), add=TRUE, col = 'white', pch=20)
+st_intersects(depthcells, predictivedomain, sparse=FALSE) |> 
+  apply(1, any) |> 
+  (\(x) !x)() |> 
+  sum() # 206 depth cells centers that do not intersect AR5 myr AND study area
+
 vars <- names(predictors)
-df <- as.data.frame(ar5cells, na.rm=TRUE)
+df <- as.data.frame(mask(predictors, predictivedomain), na.rm=TRUE)
 summary(df)
 
 df %>% 
@@ -118,8 +124,8 @@ set.seed(42)
 KL <- pmap_dbl(res, calcKL, .progress = TRUE)
 res <- res %>% 
   mutate(KL = KL)
-write_csv(res, file="output/sampleSizeKL-25preds.csv")
-# res <- read_csv("output/sampleSizeKL-25preds.csv")
+write_csv(res, file="output/Skrim/sampleSizeKL-25preds.csv")
+# res <- read_csv("output/Skrim/sampleSizeKL-25preds.csv")
 
 ggplot(res) +
   geom_point(mapping = aes(x=n, y = KL))
@@ -144,7 +150,7 @@ sn <- tibble(x, y) %>%
   filter(y >= 0.95) %>% 
   slice_head(n = 1) %>% 
   pull(x)
-sn # 285
+sn # 426
 
 plot(x, y, xlab="cLHS sample size", ylab= "normalised KL", type="l", lwd=2)
 x1<- c(-1, 4000); y1<- c(0.95, 0.95)
@@ -153,8 +159,11 @@ lines(x1,y1, lwd=2, col="red")
 x2<- c(sn, sn); y2<- c(0, 1)
 lines(x2,y2, lwd=2, col="red")
 
-dfrealized <- extract(ar5cells, depthcells, ID = FALSE) |> 
+dfrealized <- depthcells |> 
+  st_drop_geometry() |> 
+  select(all_of(vars)) |> 
   drop_na()
+all(names(dfcovs) == names(dfrealized))
 KLrealized <- mean(map2_dbl(dfcovs, dfrealized, kl.easy, nb = nb))
 KLrealizednormalized = 1 - (KLrealized-min(jj))/(max(jj)-min(jj))
 
@@ -162,29 +171,33 @@ sn_realized <- tibble(x, y) %>%
   filter(y >= KLrealizednormalized) %>% 
   slice_head(n = 1) %>% 
   pull(x)
-points(sn_realized, KLrealizednormalized) # c(212, 0.941)
-nrow(dfrealized) # 1550
+points(sn_realized, KLrealizednormalized) # c(115, 0.854)
+nrow(dfrealized) # 372
 
 # Collinearity ####
 
-df <- st_read("output/modeling.gpkg", "dataframe") |> 
+df <- st_read("output/Skrim/modeling.gpkg", "dataframe") |> 
   st_drop_geometry() |> 
   as_tibble()
 metadata <- c("sourceProbe", "sourceGPR", "sourceWisen2021", "sourceMyrarkivet")
-any(!complete.cases(df))
+auxiliary <- c("ar5cover","ar5soil","dmkdepth")
+df |> 
+  select(!any_of(c(metadata, auxiliary))) |> 
+  complete.cases() |> 
+  all()
 
-select(df, !any_of(metadata)) |> 
+select(df, !any_of(c(metadata, auxiliary))) |> 
   slice_sample(n = 1000) |> 
-  pairs(pch='.')
-corrplot::corrplot(cor(select(df, !any_of(metadata)) ), 
+  GGally::ggpairs()
+corrplot::corrplot(cor(select(df, !any_of(c(metadata, auxiliary)))), 
                    method = 'ellipse', diag = F)
 
 dflong <- df |> 
-  select(!any_of(metadata)) |> 
+  select(!any_of(c(metadata, auxiliary))) |> 
   pivot_longer(cols = -1, names_to = "covariate")
 
 ggplot(dflong) + 
   geom_point(aes(x = value, y = depth_cm), pch=20) + 
   facet_wrap(facets = vars(covariate), scales = "free_x") +
   theme_minimal() 
-ggsave("output/exploratoryDataAnalysis.svg", height = 10, width = 20, unit="in")
+ggsave("output/Skrim/exploratoryDataAnalysis.svg", height = 10, width = 20, unit="in")
