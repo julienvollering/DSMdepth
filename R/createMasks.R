e0436bc1fc279473ad6668d2308a777109dba4bf
Julien Vollering <julienvollering@gmail.com>
2024-09-18 10:37:00 +0200

    ar5 composition

--- R/createMasks.R
+++ R/createMasks.R
@@ -14,12 +14,21 @@ sea_mask <- filter(ar5, arealtype == 82) |>
 sa <- st_difference(st_intersection(RAD_10m_mask, DTM_mask), sea_mask) |> 
   st_geometry()
 plot(sa)
+st_area(sa) |> 
+  units::set_units("km^2")
 st_write(sa, "data/Orskogfjellet-site.gpkg", "mask_studyarea", 
          append = FALSE)
 
 # AR5 myr ####
 
 ar5 <- st_transform(ar5, st_crs(RAD_10m_mask))
+st_intersection(ar5, sa) |> 
+  mutate(area = st_area(geom)) |> 
+  st_drop_geometry() |> 
+  group_by(arealtype) |> 
+  summarize(area = units::set_units(sum(area), "km^2")) |> 
+  mutate(percent = area / sum((area)) * 100)
+
 ar5.myr <- dplyr::filter(ar5, arealtype == 60) |> 
   st_union() |> 
   st_cast("POLYGON") |> 
