79a51f62bba628c6ed257d7ee787dfb7ed635f97
Julien Vollering <julienvollering@gmail.com>
2024-10-31 00:00:50 +0100

    ms: add depthsByClass table

--- ms/tables.R
+++ ms/tables.R
@@ -1,6 +1,6 @@
-library(tidyverse)
-
 # Predictors table ####
+
+library(tidyverse)
 
 rast <- terra::rast("output/predictors.tif")
 names(rast)
@@ -33,6 +33,44 @@ tribble(
   "DTW160000", "Depth-to-water index, flow initiation area of 16 ha",
   "DMK", "DMK peat depth class, categorical with 3 levels") |> 
   write_csv("ms/tables/predictors.csv")
+
+# Summary cell depths by attributes ####
+
+library(tidyverse)
+
+skrim <- sf::st_read("output/Skrim/modeling.gpkg", layer="dataframe") |> 
+  sf::st_drop_geometry()
+orskog <- sf::st_read("output/modeling.gpkg", layer="dataframe") |> 
+  sf::st_drop_geometry()
+sites <- bind_rows(skrim = skrim, orskog = orskog, .id = "site") |> 
+  mutate(dmkdepth = case_when(
+    dmkdepth == "djup myr" ~ "djup",
+    dmkdepth == "grunn myr" ~ "grunn",
+    TRUE ~ dmkdepth
+  ))
+overall <- sites |> 
+  group_by(site) |> 
+  summarize(n = n(), depth_cm = mean(depth_cm)) |> 
+  pivot_wider(names_from = 'site', values_from = c(n, depth_cm), 
+              names_vary = "slowest")
+byAR5 <- sites |> 
+  group_by(site, ar5cover) |> 
+  summarize(n = n(), depth_cm = mean(depth_cm)) |> 
+  mutate(prop = n / sum(n)) |>
+  filter(!(ar5cover %in% c("11", "12"))) |>
+  pivot_wider(names_from = 'site', values_from = c(n, prop, depth_cm), 
+              names_vary = "slowest")
+byDMK <- sites |> 
+  group_by(site, dmkdepth) |> 
+  summarize(n = n(), depth_cm = mean(depth_cm)) |> 
+  mutate(prop = n / sum(n)) |>
+  pivot_wider(names_from = 'site', values_from = c(n, prop, depth_cm), 
+              names_vary = "slowest")
+bind_rows(overall, byAR5, byDMK) |> 
+  select(ar5cover, dmkdepth, n_skrim, prop_skrim, depth_cm_skrim,
+         n_orskog, prop_orskog, depth_cm_orskog) |> 
+  write_csv("ms/tables/celldepth-attribute.csv")
+
 
 # sessionInfo ####
 
